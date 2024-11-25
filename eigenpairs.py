e18d896fc5e83aa673deb78b16b60dbb45877fd4
jzedaka <35489421+jzedaka@users.noreply.github.com>
2024-11-25 18:52:08 +0200

    Update eigenpairs.py

--- eigenpairs.py
+++ eigenpairs.py
@@ -2,24 +2,23 @@ import numpy as np
 
 
 def tensor_power_iteration(T, tol=1e-10, max_iter=10_000):
-    """
-    """
+    """ """
     n = T.shape[0]
     eigenpairs = []
 
     # Power iteration for each eigenpair
-    for _ in range(T.shape[0]*2):
+    for _ in range(T.shape[0] ** 3):
         # Initialize a random vector
         v = np.random.rand(n)
-        v /= np.linalg.norm(v) + 1e-8
+        v /= np.linalg.norm(v)
 
-        for iteration in range(max_iter):
+        for _ in range(max_iter):
             # Tensor apply
-            Tv = np.einsum('ijk,j,k->i', T, v, v)
+            Tv = np.einsum("ijk,j,k->i", T, v, v)
             l = np.dot(v, Tv)
 
             # Normalize the eigenvector
-            v_new = Tv / np.linalg.norm(Tv)
+            v_new = Tv / np.linalg.norm(Tv) + 1e-10
 
             # Check convergence
             if np.linalg.norm(v - v_new) < tol:
@@ -28,7 +27,6 @@ def tensor_power_iteration(T, tol=1e-10, max_iter=10_000):
             v = v_new
 
         eigenpairs.append((l, v))
-
-        T = T - l * np.einsum('i,j,k->ijk', v, v, v)
+        T = T - l * np.einsum("i,j,k->ijk", v, v, v)
 
     return eigenpairs
