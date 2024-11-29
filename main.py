986a7c62c2db92afe5cb209c7d434517b833aceb
jzedaka <35489421+jzedaka@users.noreply.github.com>
2024-11-29 19:18:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,58 @@
 import numpy as np
+import traceback
 from blvep import BLVEP
 from utils import init_model, sample_x, calc_loss
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def perform_experiment(d=3, m=5, n_steps=10):
+def perform_experiment(d, m, n_steps, rng):
 
     results = dict()
-    h_mean, h_cov, W = init_model(d=d, m=m)
+    h_mean, h_cov, W = init_model(d=d, m=m, rng=rng)
     print(W.T)
-    for sigma in [0.1, 0.4, 0.8]:
+    for sigma in [0.1, 0.2, 0.4, 0.8]:
         results[sigma] = dict()
         print("--------------------------")
-        blvep_alg = BLVEP(d=d, sigma=sigma, tau=0, fillter_method='ks', wls_K=d)
-        Ns = np.array([10e3, 10e4], dtype=np.int32)
+        k = d
+        blvep_alg = BLVEP(d=d, sigma=sigma, tau=0, fillter_method='ks', wls_K=k, rng=rng)
+        Ns = np.array([10e2, 10e3, 10e4], dtype=np.int32)
         for n in Ns:
             errors = []
+            errors_wls = []
             sucsess = 0
-            for _ in range(n_steps):
-                X = sample_x(h_mean=h_mean, h_cov=h_cov, W=W, n=n, sigma=sigma)
-                X_fillter = sample_x(h_mean=h_mean, h_cov=h_cov, W=W, n=n, sigma=sigma)
+            while sucsess < n_steps:
+                X1 = sample_x(h_mean=h_mean, h_cov=h_cov, W=W, n=n, sigma=sigma, rng=rng)
+                X2 = sample_x(h_mean=h_mean, h_cov=h_cov, W=W, n=n, sigma=sigma, rng=rng)
                 try:
-                    W_hat = blvep_alg.recover_W(X=X, X_fillter=X_fillter)
+                    W_hat, W_wls = blvep_alg.recover_W(X1=X1, X2=X2)
                     sucsess += 1
                     assert W_hat.shape == W.T.shape, W_hat.shape
                     loss, _ = calc_loss(W.T, W_hat)
+                    loss_wls, _ = calc_loss(W.T, W_wls)
                     errors.append(loss)
-                except:
+                    errors_wls.append(loss_wls)
+                except Exception as e:
                     pass
             
             results[sigma][n] = np.array(errors)
             print(f"sigma = {sigma},  n = {n}, error = {np.mean(errors)} +- {np.std(errors)}")
+            print(f"WLS - sigma = {sigma},  n = {n}, error = {np.mean(errors_wls)} +- {np.std(errors_wls)}")
+
 
     return results
 
 def main():
+    seed = 1234
+    rng = np.random.RandomState(seed)
     results = dict()
-    for d in [1]:
-        m = d * 5
+    for d in [1, 2, 3]:
+        m = 10
         print('^^^^^^^^^^^^^^^^^')
         print(f'd = {d}, m = {m}')
         print('^^^^^^^^^^^^^^^^^')
 
-        r = perform_experiment(d=d, m=m, n_steps=1)
+        r = perform_experiment(d=d, m=m, n_steps=10, rng=rng)
         results[d] = r
 
 
